017a86ccfa2c74c031ec72e15b3b5960283a7b36
Ferenc Magyar <magyar@mail.thot-soft.com>
2009-11-17 12:33:29 +0000

    update

---
+++ compass2/core/src/main/java/no/ovitas/compass2/service/impl/XMLConfigManagerImpl.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package no.ovitas.compass2.service.impl;
+
+import no.ovitas.compass2.service.ConfigurationManager;
+
+/**
+ * @author magyar
+ *
+ */
+public class XMLConfigManagerImpl implements ConfigurationManager {
+
+	/* (non-Javadoc)
+	 * @see no.ovitas.compass2.service.ConfigurationManager#getConfigParameter(java.lang.String)
+	 */
+	@Override
+	public String getConfigParameter(String parameterName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
