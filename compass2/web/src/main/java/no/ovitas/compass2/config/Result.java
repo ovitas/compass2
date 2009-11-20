033aaa4f15ce882a4129d88e429c0896617f2190
Attila Csanyi <csanyi@mail.thot-soft.com>
2009-11-20 13:33:39 +0000

    update

--- compass2/web/src/main/java/no/ovitas/compass2/config/Result.java
+++ compass2/web/src/main/java/no/ovitas/compass2/config/Result.java
@@ -1,4 +1,7 @@
 package no.ovitas.compass2.config;
+
+import org.apache.log4j.Logger;
+
 /**
  * @author csanyi
  * 
@@ -7,6 +10,7 @@ public class Result {
 
 	// Attributes
 
+	private Logger logger = Logger.getLogger(this.getClass());
 	protected String resultThreshold;
 
 	// Getter / setter methods
