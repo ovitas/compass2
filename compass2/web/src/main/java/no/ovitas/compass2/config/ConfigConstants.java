b920c9cad0a9c8ef5f6d58251b8214ddd49129b7
Attila Csanyi <csanyi@mail.thot-soft.com>
2009-11-20 13:49:40 +0000

    Add xml tag constants

--- compass2/web/src/main/java/no/ovitas/compass2/config/ConfigConstants.java
+++ compass2/web/src/main/java/no/ovitas/compass2/config/ConfigConstants.java
@@ -9,4 +9,15 @@ public class ConfigConstants {
 
 	public static final String COPMASS2_CONFIG = "compass2-config";
 	public static final String FULL_TEXT_SEARCH = "full-text-search";
+	public static final String CONTENT_INDEXER_IMPLEMENTATION = "content-indexer-implementation";
+	public static final String LANGUAGE_TOOLS = "language-tools";
+	public static final String LANGUAGE_TOOLS_IMPLEMENTATION = "language-tools-implementation";
+	public static final String KNOWLEDGE_BASES = "knowledge-bases";
+	public static final String KNOWLEDGE_BASE = "knowledge-base";
+	public static final String KNOWLEDGE_BASE_IMPLEMENTATION = "knowledge-base-implementation";
+	public static final String EXPANSION = "expansion";
+	public static final String ASSOCIATION_TYPES = "association-types";
+	public static final String ASSOCIATION_TYPE = "association-type";
+	public static final String RESULT = "result";
+	
 }
