38eb1c9b6c6000b15a9c5664d9ea80c67b50c8c8
Attila Csanyi <csanyi@mail.thot-soft.com>
2009-11-24 09:23:09 +0000

    revert

--- compass2/core/src/main/java/no/ovitas/compass2/service/impl/XMLConfigurationManagerImpl.java
+++ compass2/core/src/main/java/no/ovitas/compass2/service/impl/XMLConfigurationManagerImpl.java
@@ -3,9 +3,6 @@
  */
 package no.ovitas.compass2.service.impl;
 
-import java.util.Properties;
-import java.util.ResourceBundle;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -14,28 +11,15 @@ import no.ovitas.compass2.config.Compass2ConfigurationHandler;
 import no.ovitas.compass2.service.ConfigurationManager;
 
 /**
- * @author magyar
+ * @author magyar0
  *
  */
 public class XMLConfigurationManagerImpl implements ConfigurationManager {
 
 	protected Compass2ConfigurationHandler  handler = null;
 	protected Compass2Configuration configuration = null;
-	private ResourceBundle rb = null;
-	private Log log = LogFactory.getLog(getClass());
 
-	// Constructor, initialize handler
-	
-	public XMLConfigurationManagerImpl() {
-		
-		// Get configuration file location
-		rb = ResourceBundle.getBundle("compass2");
-		String configPath = rb.getString("config.file.location");
-		
-		handler = new Compass2ConfigurationHandler();
-		handler.initialize(configPath);
-		configuration = handler.getConfig();
-	}
+	private Log log = LogFactory.getLog(getClass());
 	
 	/* (non-Javadoc)
 	 * @see no.ovitas.compass2.service.ConfigurationManager#getConfigParameter(java.lang.String)
