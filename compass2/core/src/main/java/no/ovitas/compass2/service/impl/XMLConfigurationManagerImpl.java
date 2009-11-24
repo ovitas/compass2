50246d1959f3712b93a08706cdb654f98c9c7998
Attila Csanyi <csanyi@mail.thot-soft.com>
2009-11-24 08:22:40 +0000

    Create constructor to load config file

--- compass2/core/src/main/java/no/ovitas/compass2/service/impl/XMLConfigurationManagerImpl.java
+++ compass2/core/src/main/java/no/ovitas/compass2/service/impl/XMLConfigurationManagerImpl.java
@@ -4,6 +4,7 @@
 package no.ovitas.compass2.service.impl;
 
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -19,10 +20,22 @@ import no.ovitas.compass2.service.ConfigurationManager;
 public class XMLConfigurationManagerImpl implements ConfigurationManager {
 
 	protected Compass2ConfigurationHandler  handler = null;
-	protected Compass2Configuration configuration = null; 
+	protected Compass2Configuration configuration = null;
+	private ResourceBundle rb = null;
 	private Log log = LogFactory.getLog(getClass());
 
+	// Constructor, initialize handler
 	
+	public XMLConfigurationManagerImpl() {
+		
+		// Get configuration file location
+		rb = ResourceBundle.getBundle("compass2");
+		String configPath = rb.getString("config.file.location");
+		
+		handler = new Compass2ConfigurationHandler();
+		handler.initialize(configPath);
+		configuration = handler.getConfig();
+	}
 	
 	/* (non-Javadoc)
 	 * @see no.ovitas.compass2.service.ConfigurationManager#getConfigParameter(java.lang.String)
