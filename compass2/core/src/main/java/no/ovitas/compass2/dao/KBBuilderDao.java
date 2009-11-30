f50aba03f09cebc7564b4e8085d30fb3c813fa72
Christopher Kolstad <chriswk@ovitas.no>
2009-11-30 16:16:39 +0000

    Move to trunk folder

--- compass2/core/src/main/java/no/ovitas/compass2/dao/KBBuilderDao.java
+++
@@ -1,8 +0,0 @@
-package no.ovitas.compass2.dao;
-
-import no.ovitas.compass2.model.KnowledgeBaseHolder;
-
-public interface KBBuilderDao {
-
-	public KnowledgeBaseHolder buildKB(String kbAccessString);
-}
