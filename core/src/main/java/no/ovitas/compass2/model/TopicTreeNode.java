f3f8e009944182318fa56fe839d80924ddbd63e4
Attila Csanyi <csanyi@mail.thot-soft.com>
2009-12-03 12:22:24 +0000

    Add flag attribute and two new class method

--- core/src/main/java/no/ovitas/compass2/model/TopicTreeNode.java
+++ core/src/main/java/no/ovitas/compass2/model/TopicTreeNode.java
@@ -9,12 +9,14 @@ public class TopicTreeNode extends Topic {
 	private TopicTreeNode parent;
 	private double boost;
 	private HashMap<Topic, TopicTreeNode> children = new HashMap<Topic, TopicTreeNode>(0);
+	private boolean flag;
 	
 	public TopicTreeNode(Topic src, double boost) {
 		super(src);
 		this.boost = boost;
+		this.flag = false;
 	}
-	
+
 	public void setParent(TopicTreeNode parent) {
 		this.parent = parent;
 		if (parent != null) {
@@ -34,6 +36,40 @@ public class TopicTreeNode extends Topic {
 		children = newNodes;
 	}
 	
+	/**
+	 * Remove not flagged node of the current node
+	 */
+	public void truncate() {
+		
+		// If this node is flagged
+		if (this.isFlag())
+		{
+			// Iterate over the node and remove not flagged nodes
+			for (TopicTreeNode child : children.values()) {
+				child.truncate();
+			}
+		} else {
+			// Remove this from it parent children collection
+			parent.removeChildNode(this);
+		}
+		
+	}
+	
+	/**
+	 * Remove child node from children collection 
+	 * @param child
+	 */
+	public void removeChildNode(TopicTreeNode child) {
+		HashMap<Topic, TopicTreeNode> newNodes = new HashMap<Topic, TopicTreeNode>();
+		for (TopicTreeNode oneChild : children.values()) {
+			
+			if (oneChild != child) {
+				newNodes.put(oneChild, oneChild);
+			}
+		}
+		children = newNodes;
+	}
+		
 	public TopicTreeNode getParent() {
 		return parent;
 	}
@@ -50,4 +86,12 @@ public class TopicTreeNode extends Topic {
 		this.boost = boost;
 	}
 
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	
 }
